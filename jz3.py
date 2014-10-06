4ca908acc120eac74635edca14e6b3be08a8d4e1
Willem Thiart <himself@willemthiart.com>
2014-10-06 22:44:53 +0900

    simplified

--- jz3.py
+++ jz3.py
@@ -7,10 +7,6 @@ import random
 import lmdb
 import struct
 
-import socket
-import time
-
-# try to import C parser then fallback in pure python parser.
 try:
     from http_parser.parser import HttpParser
 except ImportError:
@@ -19,13 +15,6 @@ except ImportError:
 
 from http_parser.http import HttpStream, NoMoreData
 from http_parser.reader import SocketReader
-
-
-env = None
-
-
-import multiprocessing
-from multiprocessing import reduction
 
 
 def open_db():
@@ -54,44 +43,19 @@ def http_read(s, env):
                 'Content-Length: 0\n'
                 'Server: jz/0.1.0\n\r\n\r\n')
         except NoMoreData:
-            print("done")
             break
-
-
-def worker(conn):
-    env = open_db()
-    #time.sleep(.5)
-    conn.poll(None)
-    while True:
-        s = socket.fromfd(reduction.recv_handle(conn), socket.AF_INET, socket.SOCK_STREAM)
-        s.setblocking(1)
-        http_read(s, env)
 
 
 class Server(object):
     def __init__(self):
-        self.children = []
-
-        class Child(object):
-            pass
-
-        for i in range(4):
-            c = Child()
-            c.pipe_parent, c.pipe_child = multiprocessing.Pipe()
-            c.ch = multiprocessing.Process(target=worker, args=(c.pipe_child,))
-            c.ch.start()
-            self.children.append(c)
+        pass
 
 
 server = Server()
 
 
 def echo(s, address):
-    #print('New connection from %s:%s' % address)
-    #s.sendall('Welcome to the echo server! Type quit to exit.\r\n')
-    #http_read(s)
-    child = server.children[random.randint(0, 3)]
-    reduction.send_handle(child.pipe_parent, s.fileno(), child.ch.pid)
+    http_read(s, env)
 
 
 if __name__ == '__main__':
